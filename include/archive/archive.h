010388e2c00566200d8945b725125b77cedfa298
Vincent PARRES <vincent.parres@epitech.eu>
2017-03-04 13:55:32 +0100

    Update : Added Posix header to archive and unarchive .h

--- include/archive/archive.h
+++ include/archive/archive.h
@@ -5,10 +5,31 @@
 ** Login   <tails@epitech.net>
 **
 ** Started on  Fri Mar  3 21:45:07 2017 Miles PROWER
-** Last update Fri Mar  3 21:56:39 2017 Miles PROWER
+** Last update Sat Mar  4 13:54:49 2017 Miles PROWER
 */
 
 #ifndef ARCHIVE_H_
 # define ARCHIVE_H_
+
+typedef struct	s_posix_header
+{                              /* byte offset */
+  char	name[100];               /*   0 */
+  char	mode[8];                 /* 100 */
+  char	uid[8];                  /* 108 */
+  char	gid[8];                  /* 116 */
+  char	size[12];                /* 124 */
+  char	mtime[12];               /* 136 */
+  char	chksum[8];               /* 148 */
+  char	typeflag;                /* 156 */
+  char	linkname[100];           /* 157 */
+  char	magic[6];                /* 257 */
+  char	version[2];              /* 263 */
+  char	uname[32];               /* 265 */
+  char	gname[32];               /* 297 */
+  char	devmajor[8];             /* 329 */
+  char	devminor[8];             /* 337 */
+  char	prefix[155];             /* 345 */
+  /* 500 */
+}		t_posix_header;
 
 #endif /* !ARCHIVE_H_ */
