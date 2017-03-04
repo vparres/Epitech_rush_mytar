202b1113fd44f2969c0ea35644799d20ea86f2ee
keller <adrien.keller@epitech.eu>
2017-03-04 17:10:43 +0100

    Update : copy of source to tar

--- src/archive/create_file.c
+++ src/archive/create_file.c
@@ -5,12 +5,14 @@
 ** Login   <adrien.keller@epitech.eu>
 **
 ** Started on  Sat Mar  4 14:14:06 2017 Adrien KELLER
-** Last update Sat Mar  4 15:46:28 2017 Adrien KELLER
+** Last update Sat Mar  4 17:09:02 2017 Adrien KELLER
 */
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 #include "archive/archive.h"
 
@@ -18,19 +20,33 @@ int	create_file(int argc, char **argv, int opt1)
 {
   struct stat	st;
   int	i;
+  int	fd;
+  int	tar;
+  char *buff;
 
   if (opt1 == 2)
     i = 2;
   else
     i = 1;
-  fopen(argv[i], "w");
+  fopen(argv[i], "wr");
+  tar = open(argv[i], O_RDWR);
   i++;
   while (i != argc)
   {
     stat(argv[i], &st);
     printf("size of file %i : %ld\n", i, st.st_size);
+    buff = malloc(sizeof(char) * st.st_size + 1);
+    fd = open(argv[i], O_RDWR);
+    read(fd, buff, st.st_size);
+    create_header(tar, fd);
+    write(tar, buff, st.st_size);
     i++;
   }
-  //create_header();
+  return (0);
+}
+
+int	create_header(int tar, int fd)
+{
+  write(tar, "[INSERER LE HEADER POSIX]\n\n\n", 28);
   return (0);
 }
