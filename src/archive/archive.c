2ddd2c6e1c4fab0507ab7d8c89f07f2337837a16
keller <adrien.keller@epitech.eu>
2017-03-04 14:20:10 +0100

    add 'create_file'

--- src/archive/archive.c
+++ src/archive/archive.c
@@ -5,12 +5,27 @@
 ** Login   <tails@epitech.net>
 **
 ** Started on  Fri Mar  3 21:42:28 2017 Miles PROWER
-** Last update Fri Mar  3 21:53:38 2017 Miles PROWER
+** Last update Sat Mar  4 14:13:46 2017 Adrien KELLER
 */
 
-#include "archive/archive.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "../../include/archive/archive.h"
 
-int	main()
+int	main(int argc, char *argv[])
 {
+  struct stat	st;
+
+  if (argc < 2)
+  {
+    write(2, "Not enough arguments\n", 21);
+    return (84);
+  }
+  stat(argv[2], &st);
+  printf("size : %i\n", st.st_size);
+  create_file(argv);
+
   return (0);
 }
---
+++ src/archive/create_file.c
@@ -0,0 +1,9 @@
+/*
+** create_file.c for rush1 in /home/adrien/rendu/CPE_2016_b2rush1/src/archive/
+**
+** Made by Adrien KELLER
+** Login   <adrien.keller@epitech.eu>
+**
+** Started on  Sat Mar  4 14:14:06 2017 Adrien KELLER
+** Last update Sat Mar  4 14:14:11 2017 Adrien KELLER
+*/
